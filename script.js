11e9c33033195cef9aa908846b6a3a372b143a81
Deborah Kurz <DeborahKurz6@gmail.com>
2023-06-24 14:33:07 -0600

    sidebar fixed

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const h2 = document.createElement("h2");
-h2.textContent = "...";
-
 document.querySelector("body").appendChild(h2);
+h2.textContent = "My name is Deborah,"
+
+const h4 = document.createElement("h4");
+document.querySelector("h2").appendChild(h4);
+h4.textContent = "and these are my Software Engineering projects!"
