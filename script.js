27d837c5a845cbe7badb9638f98740fef9846020
Deborah Kurz <DeborahKurz6@gmail.com>
2023-06-25 09:35:13 -0600

    Added project images

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ h2.textContent = "My name is Deborah,"
 const h4 = document.createElement("h4");
 document.querySelector("h2").appendChild(h4);
 h4.textContent = "and these are my Software Engineering projects!"
+
+const input = document.getElementById('button');
+input.addEventListener('click', function(){
+    alert('I was clicked!');
+})
