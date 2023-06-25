8bf734dba2a6200740784391c21a2dd0a7f5cd6a
Deborah Kurz <DeborahKurz6@gmail.com>
2023-06-25 16:39:50 -0600

    Aligned Center Project Pics

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ const h4 = document.createElement("h4");
 document.querySelector("h2").appendChild(h4);
 h4.textContent = "and these are my Software Engineering projects!"
 
-const input = document.getElementById('button');
+const imageOne = document.getElementById('imageOne');
 input.addEventListener('click', function(){
     alert('I was clicked!');
 })
+
+const event = document.getElementById("Coming Soon");
+document.addEventListener("click",function() {
+    alert("Coming Soon");
+})
+
